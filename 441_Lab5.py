97e6d7c8a8cdcd49b22b86369ff35682d6d06908
22nickms <nseecharan22@gmail.com>
2025-10-06 19:45:14 -0400

    Update 441_Lab5.py

--- 441_Lab5.py
+++ 441_Lab5.py
@@ -1,40 +1,43 @@
 ## Lab 5 - Pulse Width Modulation (PWM) and Threaded Callbacks ## 
-##----------------------------------------------------------------##
-
-## Part 1 time.time() function
-
+# Import Modules
 import RPi.GPIO as GPIO
 import time # import time module
 import math # import math module
 
+# Assignment of GPIO pins to LED pins
 GPIO_pins = [2, 3, 4, 17, 27, 22, 10, 9, 11, 0] # List of 10 LED pins
 for i in GPIO_pins: 
 	GPIO.setup(i, GPIO.OUT)
 
+# Declaration of key variables in Brightness Equation (f, phi)
 f = 0.2 # frequency in [Hz]
-phi = math.pi / 11 # phase angle in [rad]
+#phi = math.pi / 11 # phase angle in [rad]
+phi_increase = math.pi / 11 # incremental step in [rad]
 
-GPIO_pwm = [
+# PWM Declaration with Base Frequency
+GPIO_pwm = [GPIO.PWM(i, 500) for i in GPIO_pins]
+for pwm in GPIO_pwm:
+	pwm.start(0)
 
+# try-except-finally block to run traversing LED path
 try:
 	start = time.time() # Utilizes time.time() to initialize start time
 	while True:
-		t = time.time() - start # records current time elapsed
-		b1 = (math.sin(2 * math.pi * f * t)) ** 2 # Brightness function
-		duty1 = b1 * 100 # Duty cycle to measure from 0-100%
-		pwm1.ChangeDutyCycle(duty1) # LEC 4 pwm 
-
-		b2 = (math.sin(2 * math.pi * f * t - phi)) ** 2
-		duty2 = b2 * 100
-		pwm2.ChangeDutyCycle(duty2)
+		t = time.time() - start
+		for i, n in GPIO_PWM:
+			phi = n * phi_increase
+			b = (math.sin(2 * math.pi * f * t - phi)) ** 2
+			duty = b * 100
+			pwm.ChangeDutyCycle(duty) # STOPPED HERE
 except KeyboardInterrupt:
 	print('\n Program Shutting Down')
 	pass
 finally:
-	pwm1.stop()
-	pwm2.stop()
+	for pwm in GPIO_pwm:
+		pwm.stop()
 	GPIO.cleanup()
 ## ----------------------------------------------------------------##
+
 
 
 
