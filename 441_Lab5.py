ddf2a6d146117a3301f6e376d84f2f0651192f9f
22nickms <nseecharan22@gmail.com>
2025-10-06 18:51:37 -0400

    Update 441_Lab5.py

--- 441_Lab5.py
+++ 441_Lab5.py
@@ -30,27 +30,3 @@ finally:
 	pwm.stop()
 	GPIO.cleanup()
 ## ----------------------------------------------------------------##
-
-## Part 2. Second LED
-
-import RPi.GPIO as GPIO
-import time
-import math
-
-GPIO.setmode(GPIO.BCM)
-p = 4
-p2 = 5 # Second LED
-GPIO.setup(p, GPIO.OUT)
-GPIO.setup(p2, GPIO.OUT)
-
-
-phi = math.pi / 11 # phase angle in [radians]
-f = 0.2 # frequency in [Hz]
-
-pwm = GPIO.PWM(p, 500) #creates a PWM object (p) with frequency of 500 Hz
-pwm.start(0)
-
-try:
-	start = time.time() # Utilizes time.time() to initialize start time
-	while True: 
-		t = time.time() - start # records current time elapsed 
