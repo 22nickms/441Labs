7ef33be64ce3ee84945f506372831ca48243da73
22nickms <nseecharan22@gmail.com>
2025-10-06 19:03:41 -0400

    Update 441_Lab5.py

--- 441_Lab5.py
+++ 441_Lab5.py
@@ -8,10 +8,13 @@ import time
 import math
 
 GPIO.setmode(GPIO.BCM)
-p = 4
+p = 2
 GPIO.setup(p, GPIO.OUT)
+p2 = 3
+GPIO.setup(p2, GPIO.OUT)
 
 f = 0.2 # frequency in [Hz]
+phi = math.pi / 11 # phase angle in [rad]
 
 pwm = GPIO.PWM(p, 500) #creates a PWM object (p) with frequency of 500 Hz
 pwm.start(0)
@@ -20,7 +23,7 @@ try:
 	start = time.time() # Utilizes time.time() to initialize start time
 	while True:
 		t = time.time() - start # records current time elapsed
-		b = (math.sin(2 * math.pi * f * t)) ** 2 # Brightness function
+		b = (math.sin(2 * math.pi * f * t - phi)) ** 2 # Brightness function
 		duty = b * 100 # Duty cycle to measure from 0-100%
 		pwm.ChangeDutyCycle(duty) # LEC 4 pwm 
 except KeyboardInterrupt:
@@ -30,3 +33,4 @@ finally:
 	pwm.stop()
 	GPIO.cleanup()
 ## ----------------------------------------------------------------##
+
