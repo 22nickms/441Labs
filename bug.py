9fb1a85656c9c8c1dc70f940f4241ad711d3242b
22nickms <nseecharan22@gmail.com>
2025-10-19 23:38:35 -0400

    Add files via upload

---
+++ bug.py
@@ -0,0 +1,48 @@
+from shifter import Shifter
+import RPi.GPIO as GPIO
+from time import sleep
+import random   
+from shifter import Bug
+
+GPIO.setmode(GPIO.BCM)
+
+dataPin = 23
+latchPin = 24
+clockPin = 25
+
+s1 = 5
+s2 = 6
+s3 = 13
+
+GPIO.setup(s1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(s2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(s3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+ss = shifter.shifter(dataPin, latchPin, clockPin)
+
+bug = Bug.Bug(ss)
+
+def s1_call(s1):
+    bug.active = not bug.active
+
+def s2_call(s2):
+    bug.isWrapOn = not bug.isWrapOn
+
+def s3_call(s3):
+    if GPIO.input(s3):
+        bug.timestep = bug.timestep / 3
+    else:
+        bug.timestep = bug.timestep * 3
+    
+GPIO.add_event_detect(s1, GPIO.RISING, callback=s1_call, bouncetime=300)
+GPIO.add_event_detect(s2, GPIO.RISING, callback=s2_call, bouncetime=300)
+GPIO.add_event_detect(s3, GPIO.BOTH, callback=s3_call, bouncetime=300)
+
+try:
+    while True:
+        if bug.active:
+            bug.step()
+            sleep(bug.timestep)
+except KeyboardInterrupt:
+    bug.stop()
+    GPIO.cleanup()
