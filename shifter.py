2a3beef8b655c098b4f5247f3c5c67d20ea51b01
22nickms <nseecharan22@gmail.com>
2025-10-19 23:43:43 -0400

    Update shifter.py

--- shifter.py
+++ shifter.py
@@ -1,20 +1,19 @@
 import RPi.GPIO as GPIO
 import time
+import random
 
+## Shifter Class
 class shifter:
-
 	def __init__(self, serial_pin, latch_pin, clock_pin):
 		self.serial = serial_pin 
 		self.clock = clock_pin
 		self.latch = latch_pin
 		self.start()
 
-
 	def start(self):
 		GPIO.setup(self.serial, GPIO.OUT)
 		GPIO.setup(self.latch, GPIO.OUT, initial=0)
 		GPIO.setup(self.clock, GPIO.OUT, initial=0)
-
 
 	def _ping(self,p):
 		GPIO.output(p,1)
@@ -27,7 +26,34 @@ class shifter:
 			self.__ping(self.clock)
 		self._ping(self.latch)
 
+## Bug Class 
+class Bug: 
+    def __init__(self, __shifter, timestep = 0.1, x = 3, isWrapOn = True):
+        self.__shifter = __shifter
+        self.x = self.timestep = timestep
+        self.isWrapOn = isWrapOn
+        self.__move = False
 
+    def step(self):
+        leds = 8
+        pattern = 1 << self.x
+        self.__shifter.shiftByte(pattern)
+        s = random.choice([-1,1])
+        self.x += s
 
-
-
+        if self.isWrapOn:
+            self.x = self.x % leds
+        else:
+            if self.x < 0:
+                self.x = 0
+            elif self.x > leds-1:
+                self.x = leds-1
+				
+    def start(self):
+        self.__move = True
+        while self.__move:
+            self.step()
+            time.sleep(self.timestep)
+    
+    def stop(self):
+        self.__move = False
